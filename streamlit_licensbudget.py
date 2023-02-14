7eacfc70915b050e5e7a012a52e987bde8cd8494
LiheKB <125368927+LiheKB@users.noreply.github.com>
2023-02-14 13:46:06 +0100

    Update streamlit_licensbudget.py

--- streamlit_licensbudget.py
+++ streamlit_licensbudget.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title='Dyvekes UNGLI app', page_icon="appikon.jpg", layo
 
 
 
-file = filename = r'Regnskabsdata.xls'
+file = filename = 'Regnskabsdata.XLS'
 st.sidebar.subheader('Indlæs din regnskabsdata fil')
 uploaded_file = st.sidebar.file_uploader("Skal være excel fil")#, type = 'xlsx')
 st.sidebar.subheader('Indtast UNGLI information')
