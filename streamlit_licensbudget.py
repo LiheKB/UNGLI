87486cbd81f87142eb06a27952a2f86b72b606f3
LiheKB <125368927+LiheKB@users.noreply.github.com>
2023-02-14 13:51:24 +0100

    Update streamlit_licensbudget.py

--- streamlit_licensbudget.py
+++ streamlit_licensbudget.py
@@ -53,7 +53,7 @@ metrik2 = UNGLI_belob/taxameter*100
 metrik3 = gennemforelse/taxameter*100
 
 col1, col2, col3 = st.columns(3)
-col1.metric('Andel af "Undervisningens gennemførelse, øvrige omkostninger" der består af UNGLI licenser', value = metrik1 + "%")
+col1.metric('Andel af "Undervisningens gennemførelse, øvrige omkostninger" der består af UNGLI licenser', value = str(metrik1)+ "%")
 col2.metric('Andel af undervisningstaxameter der består af UNGLI licenser', value = metrik2 + "%")
 col3.metric('Andel af undervisningstaxameter der består af "Undervisningens gennemførelse, øvrige omkostninger"', value = metrik3 + "%")
 
