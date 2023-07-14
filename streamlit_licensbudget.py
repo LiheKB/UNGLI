678a5cae65728330df861b40d72763c6e8cae465
LiheKB <lihe@kb.dk>
2023-07-14 15:59:00 +0200

    cdc

--- streamlit_licensbudget.py
+++ streamlit_licensbudget.py
@@ -79,10 +79,10 @@ if uploaded_files is not None:
 
         metrik3 = np.round(gennemforelse/taxameter*100,3)
 
-        ende_data = np.array([df['Institutions nummer:']], df.iloc[:,1], metrik1, metrik2, metrik3)
+        ende_data = np.array([df['Institutions nummer:']].to_numpy(), df.iloc[:,1].to_numpy(), metrik1, metrik2, metrik3)
         ende_kol = ["institutionsnummer", "institutionsnavn", "Andel af 'Undervisningens gennemførelse, øvrige omkostninger' der består af UNGLI licenser:", "Andel af undervisningstaxameter der består af UNGLI licenser:", "Andel af undervisningstaxameter der består af 'Undervisningens gennemførelse, øvrige omkostninger':"]
         endelig_df = pd.DataFrame(ende_data, columns = ende_kol)
-        
+
         #st.subheader('Analyse af ' + inst_navn + ', institutionsnummer: ' + str(inst_num))
         st.write(endelig_df)
         #st.metric('Andel af "Undervisningens gennemførelse, øvrige omkostninger" der består af UNGLI licenser:', value = str(metrik1)+ "%")
