46c14d6cf47f46af6d3ce067f87cf152b3c39a6b
LiheKB <lihe@kb.dk>
2023-07-14 16:00:15 +0200

    fix fix

--- streamlit_licensbudget.py
+++ streamlit_licensbudget.py
@@ -79,7 +79,7 @@ if uploaded_files is not None:
 
         metrik3 = np.round(gennemforelse/taxameter*100,3)
 
-        ende_data = np.array([df['Institutions nummer:']].to_numpy(), df.iloc[:,1].to_numpy(), metrik1, metrik2, metrik3)
+        ende_data = np.array([df['Institutions nummer:'].to_numpy(), df.iloc[:,1].to_numpy(), metrik1, metrik2, metrik3])
         ende_kol = ["institutionsnummer", "institutionsnavn", "Andel af 'Undervisningens gennemførelse, øvrige omkostninger' der består af UNGLI licenser:", "Andel af undervisningstaxameter der består af UNGLI licenser:", "Andel af undervisningstaxameter der består af 'Undervisningens gennemførelse, øvrige omkostninger':"]
         endelig_df = pd.DataFrame(ende_data, columns = ende_kol)
 
